4702e0d2a286ccb11558de1c336bdf0a2d2d83fc
mehra91 <pm7300779625@gmail.com>
2025-11-14 22:30:22 +0530

    add new code

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import Carsoul from './Components/Carsoul'
 const App = () => {
   return (
     <>
-        <Navbar/>
-        <Carsoul/>  
+      <Navbar />
+      <Carsoul />
+      <div className='h-10 w-5xl   mt-4 ml-4 flex items-center pl-5  '>
+        <h1 className='  text-xl bg-gradient-to-r bg-orange-600 to-bg-black font-semibold flex items-center justify-center '>
+          Shop by Category
+        </h1>
+      </div>  
     </>
   )
 }
