31a513bdc5040b53c01066d9c6f5e8bafaed63b7
mehra91 <pm7300779625@gmail.com>
2025-11-15 23:10:28 +0530

    add new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Carsoul from './Components/Carsoul'
+import CategoryCard from './Components/CategoryCard'
 
 const App = () => {
   return (
@@ -8,10 +9,16 @@ const App = () => {
       <Navbar />
       <Carsoul />
       <div className='h-10 w-5xl   mt-4 ml-4 flex items-center pl-5  '>
-        <h1 className='  text-xl bg-gradient-to-r bg-orange-600 to-bg-black font-semibold flex items-center justify-center '>
+        <h1 className='bg-linear-to-r from-red-400 via-red-600 to-slate-900  font-medium  bg-clip-text text-2xl text-transparent flex items-center justify-center '>
           Shop by Category
         </h1>
-      </div>  
+      </div>
+      <div className='  w-full h-auto mt-2  flex items-center justify-evenly p-10 mb-20 gap-5   flex-wrap'>
+        
+      <CategoryCard />
+      <CategoryCard/>
+       
+   </div>
     </>
   )
 }
---
+++ src/Components/CategoryCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const CategoryCard = () => {
+  return (
+  
+    <div className='bg-green-500 h-60 w-55 flex items-center justify-center rounded overflow-hidden cursor-pointer group  relative ' >
+        <img src="http://downtown.nop-station.com/images/thumbs/0003269_men_460.webp" alt="" className='h-60 w-55 rounded object-cover' />
+         <div className="
+    absolute bottom-0 left-0 w-full h-full flex flex-col items-center    text-white
+    bg-linear-to-t form-black/80 via-black/30 to-black/30  
+    translate-y-50
+    group-hover:translate-y-30
+    transition-all duration-500 ease-in-out
+    
+    ">
+        <h1 className='text-lg font-semibold   flex items-center justify-center mb-3    '>
+            Men
+        </h1>
+        <h3 className='text-sm font-normal flex items-center justify-center mb-1 z-1  ' >
+            Bottoms
+        </h3>
+        <h3 className='text-sm font-normal flex items-center justify-center mb-1 z-1  ' >
+            Clothing
+        </h3>
+        <h3 className='text-sm font-normal flex items-center justify-center mb-1 z-1  ' >
+            Shoes & Accessories
+        </h3>
+
+  </div>
+    </div>
+
+ 
+
+  )
+}
+
+export default CategoryCard;
